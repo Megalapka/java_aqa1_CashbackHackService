959d75635b14c5122cf01c80f42e703be7417a51
Megalapka <forvsykoe@list.ru>
2022-08-05 14:42:46 +0300

    junit4-platform created

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -2,6 +2,7 @@ package ru.netology.service;
 
 import org.junit.Test;
 
+
 import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
@@ -35,4 +36,39 @@ public class CashbackHackServiceTest {
 
         assertEquals(expected, actual);
     }
+
+
+
+///API JUnit Jupiter
+
+    @org.junit.jupiter.api.Test
+    public void ngShouldRemain() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(900);
+        int expected = 100;
+
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void ngShouldRemainAboveThousand() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(1200);
+        int expected = 800;
+
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void ngShouldRemainEqualsThousand() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(1000);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+
 }
